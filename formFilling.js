f0a195d001af9c17bd3b8f1ae8d91faee15ce87a
Artyzh <artyzh@inbox.ru>
2022-10-26 11:33:36 +0300

    Test for Lesha

--- formFilling.js
+++ formFilling.js
@@ -2,7 +2,7 @@ rnd1 = '#i' + String(Math.floor(Math.random() * 3) * 3 + 9);
 rnd2 = '#i' + String(Math.floor(Math.random() * 3) * 3 + 31);
 inps = ['regfoijrg', 'ergutrgoeirg', 'giubergeg', 'erihiufbnv'];
 
-form = document.querySelector("form");
+sub = document.querySelector("input[value='Отправить']");
 btn1 = document.querySelector(rnd1);
 btn2 = document.querySelector(rnd2);
 inp = document.querySelector("input[type='text']");
@@ -12,6 +12,7 @@ console.log(rnd1, rnd2);
 btn1.click();
 btn2.click();
 inp.value = inps[Math.floor(Math.random() * 4)];
-addEventListener("beforeunload", (event) => { });
-//console.log(window.onbeforeunload = null);
-form.submit();
+
+// onbeforeunload = (event) => { };
+window.onbeforeunload = null;
+sub.click()
